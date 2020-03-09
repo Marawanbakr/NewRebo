13752d5505375cae4f2cb17c0427afcd112c8001
Ahmed Younes <cahmedc@gmail.com>
2020-03-09 13:39:16 +0200

    current value fix

--- src/component/Coursform.js
+++ src/component/Coursform.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Coursform =(props)=>{
-    return(
-       <form onSubmit={props.addCourse}>
-           <input type="text" onChange={props.updateCourse}/>
-           <button type="submit">Add course</button>
-       </form>
-    )
-}
-export default Coursform ;
+const Coursform = props => {
+  return (
+    <form onSubmit={props.addCourse}>
+      <input type="text" onChange={props.updateCourse} value={props.value} />
+      <button type="submit">Add course</button>
+    </form>
+  );
+};
+export default Coursform;
